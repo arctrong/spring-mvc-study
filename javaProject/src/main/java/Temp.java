6f3e0abb4a570518c0201fee9b8cd362c02222da
someone <someone@universe.all>
2023-03-19 15:24:28 +0100

    spring-web added

---
+++ javaProject/src/main/java/Temp.java
@@ -0,0 +1,16 @@
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class Temp {
+
+    public static void main(String[] args) {
+
+        DispatcherServlet d;
+        WebApplicationContext c;
+
+
+
+    }
+
+
+}
