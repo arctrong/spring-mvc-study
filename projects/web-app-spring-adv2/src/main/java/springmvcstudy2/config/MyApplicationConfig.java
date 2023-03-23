795beda0443df37aa087cd6ac24d5cb9d093b8a2
someone <someone@universe.all>
2023-03-23 08:57:52 +0100

    project 'web-app-spring-adv2' initialized

---
+++ projects/web-app-spring-adv2/src/main/java/springmvcstudy2/config/MyApplicationConfig.java
@@ -0,0 +1,21 @@
+package springmvcstudy2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "springmvcstudy2.controllers")
+public class MyApplicationConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
