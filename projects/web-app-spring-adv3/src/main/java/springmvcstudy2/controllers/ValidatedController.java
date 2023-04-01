24df68c254b3e3173d927e87505a419c41997009
someone <someone@universe.all>
2023-04-01 16:03:29 +0200

    first simple cookie usage demonstrated

--- projects/web-app-spring-adv3/src/main/java/springmvcstudy2/controllers/ValidatedController.java
+++ projects/web-app-spring-adv3/src/main/java/springmvcstudy2/controllers/ValidatedController.java
@@ -7,25 +7,40 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springmvcstudy2.model.UserInfoValidatedDto;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
 public class ValidatedController {
 
     @RequestMapping("/validate")
-    public String showHomePageSpring(@ModelAttribute("dto") UserInfoValidatedDto dto) {
+    public String showHomePageSpring(@ModelAttribute("dto") UserInfoValidatedDto dto,
+                                     HttpServletRequest request) {
+        for (Cookie cookie : request.getCookies()) {
+            if ("myApp.userName".equals(cookie.getName())) {
+                dto.setName1(cookie.getValue());
+            }
+        }
         return "home-page-validated";
     }
 
     @RequestMapping("/validate-submit")
     public String showCalculatePageSpring(@Valid @ModelAttribute("dto") UserInfoValidatedDto dto,
-                                          BindingResult result) {
+                                          BindingResult result,
+                                          HttpServletResponse response) {
         System.out.println("showCalculatePageSpring called");
         if (result.hasErrors()) {
             System.out.println("VALIDATION: form has errors");
             result.getAllErrors().forEach(System.out::println);
             return "home-page-validated";
         }
+
+        Cookie cookie = new Cookie("myApp.userName", dto.getName1());
+        cookie.setMaxAge(24 * 60 * 60);
+        response.addCookie(cookie);
+
         return "result-page-spring";
     }
 }
