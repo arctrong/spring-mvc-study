24248224450db59173c41fd9960d39482c0cc206
someone <someone@universe.all>
2023-04-03 20:41:42 +0200

    problem with @SessionAttribute annotation

--- projects/web-app-spring-sessions/src/main/java/springmvcstudy/controllers/SecondController.java
+++ projects/web-app-spring-sessions/src/main/java/springmvcstudy/controllers/SecondController.java
@@ -3,14 +3,15 @@ package springmvcstudy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class SecondController {
 
     @RequestMapping("/normalMethod")
-    public String normalHandler(Model model) {
-        String firstName = (String) model.getAttribute("firstName");
+    public String normalHandler(@SessionAttribute("firstName") String firstName, Model model) {
         System.out.println("Inside normal handler firstName=" + firstName);
+        model.addAttribute("firstName", "Dear " + firstName);
         return "index";
     }
 }
