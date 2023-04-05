492be58a46a2f62eaa5e255dcf99abbe69e6089b
someone <someone@universe.all>
2023-04-05 17:15:29 +0200

    @ModelAttribute conbined with @SessionAttributes

--- projects/web-app-spring-sessions/src/main/java/springmvcstudy/controllers/MyWebsiteController.java
+++ projects/web-app-spring-sessions/src/main/java/springmvcstudy/controllers/MyWebsiteController.java
@@ -1,20 +1,24 @@
 package springmvcstudy.controllers;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import springmvcstudy.model.WebsiteInfoDto;
 
-import static org.apache.commons.lang3.builder.ToStringBuilder.*;
-import static org.apache.commons.lang3.builder.ToStringStyle.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+import static org.apache.commons.lang3.builder.ToStringStyle.JSON_STYLE;
 
 @Controller
 @ControllerAdvice(assignableTypes = MyTestController.class)
+@SessionAttributes("websiteInfo")
 public class MyWebsiteController {
+
+    private static AtomicInteger COUNT = new AtomicInteger(0);
 
     @ModelAttribute("websiteInfo")
     private WebsiteInfoDto getWebsiteInfo() {
@@ -37,6 +41,7 @@ public class MyWebsiteController {
     public String showCompanyInfo(@ModelAttribute("websiteInfo") WebsiteInfoDto websiteInfo) {
         System.out.println("Inside showCompanyInfo() method");
         System.out.println("websiteInfo=" + reflectionToString(websiteInfo, JSON_STYLE));
+        websiteInfo.setWebsiteCategory(websiteInfo.getWebsiteCategory() + COUNT.incrementAndGet());
         return "info";
     }
 }
