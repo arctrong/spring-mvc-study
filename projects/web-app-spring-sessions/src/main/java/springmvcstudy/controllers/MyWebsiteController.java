2ac5aecf4683500b8d12dc5f34453648537496a8
someone <someone@universe.all>
2023-04-05 12:46:13 +0200

    simple model usage demonstrated

--- projects/web-app-spring-sessions/src/main/java/springmvcstudy/controllers/MyWebsiteController.java
+++ projects/web-app-spring-sessions/src/main/java/springmvcstudy/controllers/MyWebsiteController.java
@@ -1,13 +1,19 @@
 package springmvcstudy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springmvcstudy.model.WebsiteInfoDto;
 
 @Controller
 public class MyWebsiteController {
 
-    @RequestMapping("/info")
-    public String showWebsiteInfo() {
+    @RequestMapping("/siteInfo")
+    public String showWebsiteInfo(Model model) {
+        WebsiteInfoDto websiteInfo = new WebsiteInfoDto();
+        websiteInfo.setWebsiteName("need-for-spring.edu");
+        websiteInfo.setWebsiteCategory("education");
+        model.addAttribute("websiteInfo", websiteInfo);
         return "info";
     }
 }
--- projects/web-app-spring-sessions/src/main/webapp/WEB-INF/view/info.jsp
+++ projects/web-app-spring-sessions/src/main/webapp/WEB-INF/view/info.jsp
@@ -4,12 +4,14 @@
 <html><head><meta charset="UTF-8">
 <title>Using @ModelAttribute</title>
 <style>
-body {background-color: darkslategray; color: wheat;}
+body {background-color: darkslategray; color: wheat; font-size: larger;}
 .error {color: red; position: fixed; text-align: left; margin-left: 20px;}
 </style>
 </head><body>
 
 <h1>Using @ModelAttribute at the method level</h1>
 <hr />
+<strong>Website name: </strong>${websiteInfo.websiteName}<br />
+<strong>Website category: </strong>${websiteInfo.websiteCategory}<br />
 
 </body></html>
