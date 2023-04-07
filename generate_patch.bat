3e202eabae431ab57cb732cb0ba27f6b2b5e9084
someone <someone@universe.all>
2023-04-08 00:44:03 +0200

    study notes updated

--- generate_patch.bat
+++ generate_patch.bat
@@ -9,5 +9,8 @@ if [%1]==[] (
 set OUT=doc_src\patches\%1.txt
 
 echo ^<!--VARIABLES {"title": "Patch %1"}--^> > %OUT%
-
+echo. >> %OUT%
+echo ^<strong^>Commit message:^</strong^> >> %OUT%
+call git log --format=%%%%B -n 1 %1 >> %OUT%
+echo. >> %OUT%
 git diff %1^^! | python "%GIT_DIFF_2_HTML_HOME%\gitDiff2Html.py" >> %OUT%
