234605b0cbf55d1dc956b1ee06843bb72abeb7ea
someone <someone@universe.all>
2023-05-02 13:57:18 +0200

    embedded jetty: life war - before embedding

---
+++ projects/jetty-embedded-live-war/src/main/java/jettylifewar/config/MyApplicationInitializer.java
@@ -0,0 +1,28 @@
+package jettylifewar.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class MyApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext webApplicationContext =
+                new AnnotationConfigWebApplicationContext();
+        webApplicationContext.register(MyApplicationConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet("front-controller", dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/ui/*");
+    }
+}
---
+++ projects/jetty-embedded-live-war/src/main/webapp/WEB-INF/view/hello-page.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@page isELIgnored="false"%>
+<!DOCTYPE html>
+<html><head><meta charset="UTF-8">
+<title>Hello JSP page</title>
+</head><body>
+
+<h1>Hello page</h1>
+<hr/>
+<p>The generated servlet class is:
+<code><%= this.getClass().getSimpleName() %></code></p>
+
+</body></html>
